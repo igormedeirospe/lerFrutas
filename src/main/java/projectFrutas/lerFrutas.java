8f2c2c2613221927541de3d726bc755729ae2f39
igormedeirospe <igormedeiros407@gmail.com>
2024-06-19 11:57:37 -0300

    Segundo commit

--- src/main/java/projectFrutas/lerFrutas.java
+++ src/main/java/projectFrutas/lerFrutas.java
@@ -1,2 +1,29 @@
-package projectFrutas;public class lerFrutas {
+package projectFrutas;
+
+import javax.swing.*;
+
+public class lerFrutas {
+
+    public static void main(String[] args) {
+
+
+    String frutas1;
+    String frutas2;
+    frutas1 = JOptionPane.showInputDialog("Digite o nome da fruta");
+    frutas2 = JOptionPane.showInputDialog("Digite o nome da fruta");
+
+    if(frutas1.equals("Maça") && frutas2.equals("Banana")){
+
+        JOptionPane.showMessageDialog(null, "A fruta é " + frutas1 + " e " + frutas2);
+    }else{
+        JOptionPane.showMessageDialog(null, "fruta invalida");
+    }
+
+
+
+
+
+
+    }
+
 }
